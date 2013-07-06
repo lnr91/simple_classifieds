157222c1b34da8a95a4802f3d6d2d3d67f09f097
lnreddydell <lnreddy@lnreddydell.(none)>
2013-07-06 11:36:20 +0530

    third commit with full directory structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuyNSell::Application.config.secret_token = '5b34aec9aac9fe326de951463dec206420c42fee0a3fa7246409d17d1fa93abdae4eed6ca37be9d21e8d821c70b3366e70f17bf20dbd7c26c8421bf3d5ad3c5a'
